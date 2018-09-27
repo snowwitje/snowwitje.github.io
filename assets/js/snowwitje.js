ffae916455a02322051c3d4ede72cee93e794d0c
Alissa Nedossekina (snowwitje) <alisa@purdue.edu>
2018-09-27 09:11:21 -0400

    reorganize

--- assets/js/snowwitje.js
+++ assets/js/snowwitje.js
@@ -1,4 +1,4 @@
-/*global $, jQuery*/
+/*global $, window, document */
 $(document).ready(function () {
 	'use strict';
 	var $window = $(window),
@@ -18,4 +18,54 @@ $(document).ready(function () {
 			});
 		});
 	}
+	
+	
+	$window.on('load', function () {
+		if ($body.hasClass('special')) {
+			$('#header').removeClass('extended');
+		} else {
+			$('#header').addClass('extended');
+		}
+	});
+	
+	function menuScroll() {
+		'use strict';
+
+		$('.cat-menu li').on('click', function () {
+			$('.cat-menu li').each(function (i, item) {
+				$(item).removeClass('selected');
+			});
+			$(this).addClass('selected');
+
+			var anchor = $(this).attr('data-link');
+
+			if (document.getElementById(anchor)) {
+				$([document.documentElement, document.body]).animate({
+					scrollTop: $("#" + anchor).offset().top - 100
+				}, 500);
+			}
+
+		});
+
+		$(window).scroll(function () {
+			var position = $(this).scrollTop();
+
+			$('.content-samples').each(function () {
+				var target = $(this).offset().top;
+				var id = $(this).attr('id');
+
+				if ((position + 200) >= (target)) {
+					$('.cat-menu li').each(function (i, item) {
+						$(item).removeClass('selected');
+						if ($(item).attr('data-link') === id) {
+							$(item).addClass('selected');
+						}
+					});
+				}
+			});
+		});
+	}
+	
+	menuScroll();
+	
 });
